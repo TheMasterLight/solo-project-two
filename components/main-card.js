61ea32d67af14bd31faf21595c4971affe1ccd8d
Jeff Stratford <jeffreystrat@gmail.com>
2022-07-27 11:39:21 -0600

    Created Navigation component and worked on developing the css so that the containers are all correct sizing and basic styling on other components.

---
+++ components/main-card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+export default function travelCard() {
+  return (
+    <div className="container">
+      <h1>Test to make sure this works</h1>
+    </div>
+  )
+}
---
+++ components/navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+import WorldIcon from "../public/Images/WorldIcon.svg"
+
+export default function navigation() {
+  return(
+    <nav className="flex navigation">
+      <div className="little-space">
+        <Image src={WorldIcon} />
+      </div>
+      <h2>My Travel Journal</h2>
+    </nav>
+  )
+}
