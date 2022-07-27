5b79cf5d5f4c86ad8e0b5432b98dc4a17395866d
Jeff Stratford <jeffreystrat@gmail.com>
2022-07-27 12:59:06 -0600

    Template Progression
    continued to create the template for the main recycled component.

--- components/main-card.js
+++ components/main-card.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import IslandFates from "../public/Images/ilsand_fates.jpg"
 
 export default function travelCard() {
   return (
     <div className="container">
-      <h1>Test to make sure this works</h1>
+      <div>
+        <Image src={IslandFates} alt="a map of the island of the fates" />
+      </div>
+      <h1>Island of the Fates</h1>
+      
     </div>
   )
 }
