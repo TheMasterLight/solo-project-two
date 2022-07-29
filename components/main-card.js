a0a130a1ef365c87cea29bf0c16f74146f82671d
Jeff Stratford <jeffreystrat@gmail.com>
2022-07-29 10:36:33 -0600

    Added Database and minor code fixes
    Minor code fixes included finishing up the template for the main travel component for the challenge and also creating the database with three locations based in the fantasy world that my dungeons and dragons players had traveled to.

--- components/main-card.js
+++ components/main-card.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Image from "next/image";
-import IslandFates from "../public/Images/ilsand_fates.jpg"
+import IslandFates from "../public/Images/island_fates.jpg"
+import TravelIcon from "../public/Images/Fill 219.svg"
 
 export default function travelCard() {
   return (
-    <div className="container">
-      <div>
+    <div className="container flex">
+      <div className="preview">
         <Image src={IslandFates} alt="a map of the island of the fates" />
       </div>
-      <h1>Island of the Fates</h1>
-      
+      <div>
+        <div>
+          <div>
+            <Image scr={TravelIcon} alt="small stylized map pin"/>
+          </div>
+          <a src="#">View on Map of Thylea</a>
+        </div>
+        <div>
+          <h1>Island of the Fates</h1>
+          <p>Description of Party's Events at location</p>
+        </div>
+      </div>
     </div>
   )
 }
