29980d71c431887822bc8da730c7d1e972ee1070
Jeff Stratford <jeffreystrat@gmail.com>
2022-08-03 10:49:40 -0600

    minor fixes

--- components/main-card.js
+++ components/main-card.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import TravelIcon from "../public/Images/Fill 219.svg"
+import TravelIcon from "../public/Images/Fill219.svg";
 
 export default function travelCard(props) {
   return (
     <div className="container flex">
       <div className="preview">
-        <Image src={props.map} alt = {props.alt} width={auto} height={auto}/>
+        <Image src={props.map} alt={props.alt} layout="fill" />
       </div>
       <div>
         <div>
-          <div>
-            <Image scr={TravelIcon} alt="small stylized map pin"/>
+          <div className="flex">
+            <div>
+              <Image scr={TravelIcon} alt="small stylized map pin" layout="fill"/>
+            </div>
+            <a src="#">View on Map of Thylea</a>
           </div>
-          <a src="#">View on Map of Thylea</a>
         </div>
         <div>
           <h1>{props.title}</h1>
@@ -21,5 +23,5 @@ export default function travelCard(props) {
         </div>
       </div>
     </div>
-  )
+  );
 }
